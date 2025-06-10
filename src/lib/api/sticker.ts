7569040a128c108de7b35e409223ba4b517cad48
Matias N. Sanchez <sanchezmatidev@gmail.com>
2025-06-10 18:05:48 -0300

    Se agrego mas limite para stickers

--- src/lib/api/sticker.ts
+++ src/lib/api/sticker.ts
@@ -11,7 +11,7 @@ const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
 export async function getStickers(token: Token, page?: string, limit?: string) {
   try {
     const pages = page || "1";
-    const limits = limit || "9";
+    const limits = limit || "50";
     const res = await fetch(`${API_BASE}/item?page=${pages}&limit=${limits}`, {
       method: "GET",
       headers: {
